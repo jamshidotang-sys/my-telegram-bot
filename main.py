ac86be8d1d84fc8a555c1d63c20ca1c56d09b2e4
jamshidotang-sys <jamshidotang@gmail.com>
2026-08-08 03:57:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,9 @@ async def process_video(message: types.Message):
     else:
         await message.answer("To'g'ri havola kiriting.")
 
-
+async def handle(request):
+    return web.Response(text="Bot is running!")
+    
 async def web_server():
     app = web.Application()
     app.router.add_get("/", handle)
